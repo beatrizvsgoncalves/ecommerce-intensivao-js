3280f23a10adce7632a1b61348e78ac70535ea9d
Beatriz Gonçalves <beatrizvsgoncalves.274@gmail.com>
2023-09-26 19:12:40 -0300

    atts

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "/e-commerce-intensivao-js/",
+	base: "/ecommerce-intensivao-js/",
 	plugins: [],
 	resolve: {
 		/*something*/
